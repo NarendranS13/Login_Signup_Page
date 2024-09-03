61647a4520f1fe5f715acffde0807ab781f05c0e
NarendranS13 <narendran.s13@zohomail.in>
2024-09-03 21:08:02 +0530

    Login/Sign Up Page using React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignUp from './Components/Login_Signup/LoginSignUp';
+
+function App() {
+  return (
+    <div >
+      <LoginSignUp />
+    </div>
+  );
+}
+
+export default App;
+
+
